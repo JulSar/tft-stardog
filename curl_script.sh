bad86064080cbfa3e1cc6704009f534a82dc985d
bz10bis <baptistezegre@gmail.com>
2017-12-22 15:53:01 +0100

    cUrL pLz

--- curl_script.sh
+++ curl_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://localhost:49160/test/transaction/begin"
+curl -v -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://localhost:5820/test/transaction/begin"
