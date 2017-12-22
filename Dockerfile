e45c481171d1e5e5e8efd436c1c48679b0409bea
bz10bis <baptistezegre@gmail.com>
2017-12-22 17:17:36 +0100

    ...

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM centos:centos7
 ENV STARDOG_HOME=/data/stardog
 ENV STARDOG_INSTALL_DIR = /opt/stardog
 
-RUN mkdir -p ${STARDOG_HOME}
-RUN mkdir -p ${STARDOG_INSTALL_DIR}
+RUN mkdir -p /data/stardog
+RUN mkdir -p /opt/stardog
 
 RUN yum -y update && yum clean all
 RUN yum -y install java && yum clean all
@@ -12,11 +12,9 @@ RUN yum -y install java && yum clean all
 RUN curl http://packages.stardog.com/rpms/stardog.repo > /etc/yum.repos.d/stardog.repo
 RUN yum install -y stardog && yum clean all
 
-ADD stardog-license-key.bin ${STARDOG_INSTALL_DIR}
-ADD init_stardog.sh ${STARDOG_INSTALL_DIR}
+COPY stardog-license-key.bin /opt/stardog
+COPY init_stardog.sh /opt/stardog
 
-WORKDIR ${STARDOG_HOME}
-
-CMD ${STARDOG_INSTALL_DIR}/init_stardog.sh
-
+WORKDIR /data/stardog
+CMD /opt/stardog/init_stardog.sh && /usr/sbin/init
 EXPOSE 5820
--- init_stardog.sh
+++ init_stardog.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ ! -f ${STARDOG_HOME}/stardog-license-key.bin ]; then
-  cp ${STARDOG_INSTALL_DIR}/stardog-license-key.bin ${STARDOG_HOME}/stardog-license-key.bin
+if [ ! -f /data/stardog/stardog-license-key.bin ]; then
+  cp /opt/stardog/stardog-license-key.bin /data/stardog/stardog-license-key.bin
 fi
 
 echo "[INFO] Starting Stardog server"
-${STARDOG_INSTALL_DIR}/bin/stardog-admin server start
+/opt/stardog/bin/stardog-admin server start
 echo "[INFO] Creating database <test>"
-${STARDOG_INSTALL_DIR}/bin/stardog-admin db create -n test
+/opt/stardog/bin/stardog-admin db create -n test
 echo "[INFO] Restarting Stardog"
-${STARDOG_INSTALL_DIR}/bin/stardog-admin server stop
-${STARDOG_INSTALL_DIR}/bin/stardog-admin server start --foreground
+/opt/stardog/bin/stardog-admin server stop
+/opt/stardog/bin/stardog-admin server start --foreground
 echo "[INFO] Stardog running..."
