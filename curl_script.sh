7349a8e41acb052335b19473465ef68dc48e33dd
bz10bis <baptistezegre@gmail.com>
2017-12-26 09:10:54 +0100

    localhost to ip
    Try to specify 127.0.0.1

--- curl_script.sh
+++ curl_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://localhost:5820/test/transaction/begin"
+curl -v -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://127.0.0.1:5820/test/transaction/begin"
