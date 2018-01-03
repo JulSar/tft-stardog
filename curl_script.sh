add757c3d8afa0118a041982b003e8ce2814b818
bz10bis <baptistezegre@gmail.com>
2018-01-03 13:45:31 +0100

    Remove curl verbose

--- curl_script.sh
+++ curl_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://127.0.0.1:5820/test/transaction/begin"
+curl -X POST -H "Authorization: Basic YWRtaW46YWRtaW4=" -H "text/plain" "http://127.0.0.1:5820/test/transaction/begin"
